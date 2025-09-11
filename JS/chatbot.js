b0d91ffa4a693d88fc9d4de803ba79ba017eeb90
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-11 18:34:57 -0300

    ChatBot 100% funcional

--- JS/chatbot.js
+++ JS/chatbot.js
@@ -7,7 +7,7 @@
 // ISSO SÓ É ACEITÁVEL SE O PROGRAMA PERMANECER LOCAL NO SEU COMPUTADOR.
 // NUNCA DEPLOY ISSO EM UM AMBIENTE PÚBLICO NA WEB!
 // PARA DEPLOY PÚBLICO, É IMPRESCINDÍVEL USAR UM BACKEND PROXY.
-const GEMINI_API_KEY = 'API_KEY'; // <<< SUBSTITUA PELA SUA CHAVE REAL
+const GEMINI_API_KEY = 'SUA_CHAVE'; // <<< SUBSTITUA PELA SUA CHAVE REAL
 
 // --- Configuração Inicial ---
 const usuarioLogadoEmail = sessionStorage.getItem('usuarioLogadoEmail');
@@ -234,7 +234,7 @@ async function getBotResponse(userMessage) {
     } else {
         try {
             // Requisição POST para a API Gemini do Google
-            const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`, {
+            const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
